1fc9442dd3a57d8f0b6251703e011df4094919d9
Adrian Ortiz <20220681@itla.edu.do>
2024-09-10 01:49:25 -0400

    Primer post

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Clear();
+Console.WriteLine("Hello,  esta es parte de la primera practica: ");
+Console.ResetColor();
+Console.ReadLine();
